1728c76d96c0192a6750aa4577f5b19ba1f368b3
byung ju choi <bjc30849@gmail.com>
2022-01-06 14:55:40 +0900

    update

--- src/App.js
+++ src/App.js
@@ -3,20 +3,10 @@ import { Switch as Routes, Route } from "react-router-dom";
 import FrontApp from "./front/components/App"
 import AdminApp from "./admin/components/App"
 import Login from "./admin/pages/login/Login"
-import About from "./front/pages/About/index"
-import Post from "./front/pages/Post/index"
-import Portfolio from './front/pages/Portfolio/index';
 import AOS from "aos";
 import 'aos/dist/aos.css'; // You can also use <link> for styles
 
-
-
-
-
 function App() {
-
-    
-  
     useEffect(()=> {
         AOS.init();
         AOS.refresh();
@@ -32,9 +22,6 @@ function App() {
 
                 {/* 프론트 페이지 */}
                 <Route path="/login" component={Login}/>
-                <Route path="/About" component={About}/>
-                <Route path="/Post" component={Post}/>
-                <Route path="/Portfolio" component={Portfolio}/>
             </Routes>
         </div>
     )
